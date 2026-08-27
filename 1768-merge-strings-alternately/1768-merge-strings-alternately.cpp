be7cd77a81b3c87e2b2fd3071b91c77876f4060c
amanullah1739 <amanullah1739@gmail.com>
2026-08-27 15:02:51 +0530

    Time: 0 ms (100%), Space: 8.3 MB (97.4%) - LeetHub

---
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string mergeAlternately(string word1, string word2) {
+        int word1_len = word1.size();
+        int word2_len = word2.size();
+        string ans  = "";
+        int i=0;
+        int j=0;
+        while(i<word1_len || j<word2_len){
+            
+            
+            if(i<word1_len){
+                ans += word1[i];
+                i++;
+            }
+            if(j<word2_len){
+                ans += word2[j];
+                j++;
+
+
+            }
+        }
+        return ans;
+        
+    }
+};
